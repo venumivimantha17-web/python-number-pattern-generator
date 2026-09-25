4e9c6c91a03b2b14a34ed4532fe8afcd0a53567d
L.A Venumi <venumivimantha17@gmail.com>
2026-09-25 15:46:41 +0530

    Add number pattern generator

---
+++ number_pattern.py
@@ -0,0 +1,16 @@
+def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+
+    if n < 1:
+        return "Argument must be an integer greater than 0."
+
+    result = ""
+
+    for i in range(1, n + 1):
+        result += str(i)
+
+        if i < n:
+            result += " "
+
+    return result
